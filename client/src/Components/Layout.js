3dafc88700afcc2b2a60f2efc33017a1a9e19109
sarita khanal <saritakhanal533@gmail.com>
2019-02-09 01:17:39 +0200

    first commit

---
+++ client/src/Components/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+    render() {
+        return(
+            <div>
+                main app
+            </div>
+        )
+    }
+}
+
+export default Layout;
---
+++ client/src/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Layout from './Components/Layout';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Layout>
+        foodie
+       </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
+
